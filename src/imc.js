35e42a17a355ea7b57cc8741d582cac2f536b2b9
antonio welton <antonioweltondev@gmail.com>
2026-02-24 23:26:03 -0300

    Exercícios JavaScript criados

---
+++ src/imc.js
@@ -0,0 +1,33 @@
+/*
+Elabore um algoritmo que dado o peso e a altura de um adulto, mostre sua condição de acordo com a tabela abaixo:
+
+Fórmula do IMC (Índice de Massa Corporal)
+
+IMC = peso / (alltura * altura)
+
+IMC em adultos. Condição:
+- Abaixo de 18.5 =  Abaixo do peso;
+- Entre 18.5 e 25 = Peso normal;
+- Entre 25 e 30 = Acima do peso;
+- Entre 30 e 40 = Obeso;
+- Acima de 40 = Obesidade grave;
+*/
+
+let peso = 120;
+let altura = 1.68;
+
+let imc = peso / (altura * altura);
+
+console.log(imc);
+
+if (imc < 18.5) {
+    console.log('Abaixo do peso ideal.')
+} else if (imc >= 18.5 && imc < 25) {
+    console.log('Peso normal.')
+} else if (imc >= 25 && imc < 30) {
+    console.log('Acima do peso')     
+} else if (imc >= 30 && imc < 40) {
+    console.log('Obeso.')
+} else {
+    console.log('Obesidade grave.')
+}
---
+++ src/media.js
@@ -0,0 +1,25 @@
+/*
+1) Faça um algoritmo que dado 3 notas tiradas por um aluno em um semestre da faculdade, calcule e imprima a sua média e a sua classificação conforme a tabela abaixo.
+
+Média = (nota 1 + nota 2 + nota 3) / 3;
+
+Classificação:
+- Média menor que 5, reprovação;
+- Média entre 5 e 7, recuperação;
+- Média maior que 7, passou de semestre;
+*/ 
+
+const nota1 = 5;
+const nota2 = 10;
+const nota3 = 10;
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+if (media < 5){
+    console.log('Sua média é ' + media + ' e você está reprovado!');
+} else if (media >= 5 && media <= 7) {
+    console.log('Sua média é ' + media + ' e você está em recuperação!');
+} else {
+    console.log('Sua média é ' + media + ' e você está aprovado!');
+}
+
